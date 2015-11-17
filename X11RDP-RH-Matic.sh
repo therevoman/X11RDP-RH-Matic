9a13386c9198ec78b3677e8373408cc569b7583c
Revoman <nrevo@revoweb.com>
2015-11-17 13:02:44 -0700

    fix withjpeg option to not remove --enable-fuse

--- X11RDP-RH-Matic.sh
+++ X11RDP-RH-Matic.sh
@@ -386,14 +386,9 @@ OPTIONS
 			;;
 
 		--withjpeg)
-			XRDP_CONFIGURE_ARGS="$XRDPCONFIGURE_ARGS --enable-jpeg"
+			XRDP_CONFIGURE_ARGS="$XRDP_CONFIGURE_ARGS --enable-jpeg"
 			XRDP_BUILD_DEPENDS="$XRDP_BUILD_DEPENDS libjpeg-turbo-devel"
 			;;
-    
-		--with-librfxcodec)
-		  #TARGETS="librfxcodec $TARGETS"
-			XRDP_CONFIGURE_ARGS="$XRDPCONFIGURE_ARGS --enable-librfxcodec"
-		  ;;
 
 		--tmpdir)
 			if [ ! -d "${2}" ]; then
